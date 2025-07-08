0a981ac865618adf223f955e48c71fed82febb76
Sushil Pandey <contact.sushilpandey@gmail.com>
2025-07-08 13:33:57 +0530

    Integrate dynamic folio detection into readahead paths
    - Add dynamic folio support checks to both readahead() and readpages() functions

--- src/afs/LINUX/osi_vnodeops.c
+++ src/afs/LINUX/osi_vnodeops.c
@@ -3155,6 +3155,7 @@ afs_linux_readahead(struct readahead_control *rac)
     loff_t offset;
     struct afs_lru_pages lrupages;
     struct afs_pagecopy_task *task;
+    int use_dynamic_folios = afs_detect_dynamic_folio_support();
 
     if (afs_linux_bypass_check(inode)) {
 	afs_linux_bypass_readahead(rac);
@@ -3185,6 +3186,14 @@ afs_linux_readahead(struct readahead_control *rac)
 
     while ((page = afs_readahead_page(rac)) != NULL) {
 	offset = page_offset(page);
+
+    if (use_dynamic_folios) {
+	    unsigned int optimal_order = afs_calculate_optimal_folio_order(inode, offset, 
+									   readahead_length(rac));
+	    if (optimal_order > 0) {
+		page->private = optimal_order;
+	    }
+	}
 
 	code = get_dcache_readahead(&tdc, &cacheFp, avc, offset);
 	if (code != 0) {
@@ -3240,6 +3249,7 @@ afs_linux_readpages(struct file *fp, struct address_space *mapping,
     loff_t offset;
     struct afs_lru_pages lrupages;
     struct afs_pagecopy_task *task;
+    int use_dynamic_folios = afs_detect_dynamic_folio_support();
 
     if (afs_linux_bypass_check(inode))
 	return afs_linux_bypass_readpages(fp, mapping, page_list, num_pages);
@@ -3270,6 +3280,14 @@ afs_linux_readpages(struct file *fp, struct address_space *mapping,
 	struct page *page = list_entry(page_list->prev, struct page, lru);
 	list_del(&page->lru);
 	offset = page_offset(page);
+
+    if (use_dynamic_folios) {
+	    unsigned int optimal_order = afs_calculate_optimal_folio_order(inode, offset, 
+									   num_pages * PAGE_SIZE);
+	    if (optimal_order > 0) {
+		page->private = optimal_order;
+	    }
+	}
 
 	code = get_dcache_readahead(&tdc, &cacheFp, avc, offset);
 	if (code != 0) {
