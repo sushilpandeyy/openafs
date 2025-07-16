7c88a7126cb12691d842fd889edd393d867ac6dd
Cheyenne Wills <cwills@sinenomine.net>
2025-07-16 12:25:31 -0600

    use LINUX_MULTIPATH_FOLIO define
    (I know you have done this already)

--- src/afs/LINUX/osi_vnodeops.c
+++ src/afs/LINUX/osi_vnodeops.c
@@ -2348,7 +2348,7 @@ mapping_read_page(struct address_space *mapping, struct page *page)
 #endif
 }
 
-#if !defined(HAVE_LINUX_FOLIO_ADD_LRU) && !defined(HAVE_LINUX_FILEMAP_ALLOC_FOLIO) /* Replaced LINUX_MULTIPAGE_FOLIO */
+#if !defined(LINUX_MULTIPAGE_FOLIO)
 /*
  * small compat wrapper for filemap_alloc_folio/page_cache_alloc
  */
@@ -2369,7 +2369,7 @@ afs_page_cache_alloc(struct address_space *cachemapping)
 #endif /* LINUX_MULTIPAGE_FOLIO */
 
 
-#if defined(HAVE_LINUX_FOLIO_ADD_LRU) && defined(HAVE_LINUX_FILEMAP_ALLOC_FOLIO)  /* Replaced LINUX_MULTIPAGE_FOLIO */
+#if defined(LINUX_MULTIPAGE_FOLIO)
 static unsigned int
 afs_calculate_optimal_folio_order(struct inode *inode, loff_t offset, size_t len)
 {
@@ -2393,7 +2393,7 @@ afs_calculate_optimal_folio_order(struct inode *inode, loff_t offset, size_t len
 }
 #endif 
 
-#if defined(HAVE_LINUX_FOLIO_ADD_LRU) && defined(HAVE_LINUX_FILEMAP_ALLOC_FOLIO)  /* Replaced LINUX_MULTIPAGE_FOLIO */
+#if defined(LINUX_MULTIPAGE_FOLIO)
 /* Populate a page by filling it from the cache file pointed at by cachefp
  * (which contains indicated chunk)
  * If task is NULL, the page copy occurs syncronously, and the routine
@@ -3125,14 +3125,7 @@ afs_linux_can_bypass(struct inode *ip) {
 	    return 1;
 	case LARGE_FILES_BYPASS_CACHE:
     if (i_size_read(ip) > cache_bypass_threshold) {
-/* Don't do this.  This is a user controlled threshold for reading from the cache */
-#if 0	
-#if defined(LINUX_MULTIPAGE_FOLIO)
-        if (afs_detect_dynamic_folio_support()) {
-            return 0; // Use cache with large folios
-        }
-#endif
-#endif	
+
         return 1;
     }
     AFS_FALLTHROUGH;
